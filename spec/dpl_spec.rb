f938aa4ddb26f12dccf63158a3654d5f59af6fab
Tim Tyrrell <timomitchel23@gmail.com>
2020-07-23 10:20:17 -0600

    Students are smart!

--- spec/dpl_spec.rb
+++ spec/dpl_spec.rb
@@ -112,6 +112,10 @@ RSpec.describe 'Iteration 3' do
     expect(@dpl).to respond_to(:publication_time_frame_for).with(1).argument
     charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
     harper_lee = Author.new({first_name: "Harper", last_name: "Lee"})
+    jane_eyre = charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+    professor = charlotte_bronte.write("The Professor", "1857")
+    villette = charlotte_bronte.write("Villette", "1853")
+    mockingbird = harper_lee.write("To Kill a Mockingbird", "July 11, 1960")
 
     expect(@dpl.publication_time_frame_for(charlotte_bronte)).to eq({:start=>"1847", :end=>"1857"})
     expect(@dpl.publication_time_frame_for(harper_lee)).to eq({:start=>"1960", :end=>"1960"})
